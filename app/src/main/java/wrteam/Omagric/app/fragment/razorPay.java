46bfb08598d3fcfd93b4aec2325b4ad82890c9c2
Atul18341 <atulkumar987613@gmail.com>
2021-01-06 20:30:17 +0530

    Complete App

---
+++ app/src/main/java/wrteam/Omagric/app/fragment/razorPay.java
@@ -0,0 +1,84 @@
+package wrteam.Omagric.app.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.razorpay.Checkout;
+import com.razorpay.PaymentResultListener;
+
+import org.json.JSONObject;
+
+import wrteam.Omagric.app.R;
+
+
+public class razorPay extends AppCompatActivity implements PaymentResultListener {
+    Button paybtn;
+    TextView paytext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_razor_pay);
+        Checkout.preload(getApplicationContext());
+
+//        paytext=(TextView)findViewById(R.id.paytext);
+//        paybtn=(Button)findViewById(R.id.paybtn);
+
+        paybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makepayment();
+            }
+        });
+
+    }
+
+    private void makepayment()
+    {
+
+        Checkout checkout = new Checkout();
+        checkout.setKeyID("rzp_test_tcp1ZcXtsd8jBx");
+
+       // checkout.setImage(R.drawable.logo);
+        final Activity activity = this;
+
+        try {
+            JSONObject options = new JSONObject();
+
+            options.put("name", "ALGORIAL EDCUARE");
+            options.put("description", "Reference No. #123456");
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+            // options.put("order_id", "order_DBJOWzybf0sJbb");//from response of step 3.
+            options.put("theme.color", "#3399cc");
+            options.put("currency", "INR");
+            options.put("amount", "30000");//300 X 100
+            options.put("prefill.email", "gaurav.kumar@example.com");
+            options.put("prefill.contact","7864945278");
+            checkout.open(activity, options);
+        } catch(Exception e) {
+            Log.e("TAG", "Error in starting Razorpay Checkout", e);
+        }
+    }
+
+
+    @Override
+    public void onPaymentSuccess(String s)
+    {
+        paytext.setText("Successful payment ID :"+s);
+    }
+
+    @Override
+    public void onPaymentError(int i, String s) {
+        paytext.setText("Failed and cause is :"+s);
+    }
+}
